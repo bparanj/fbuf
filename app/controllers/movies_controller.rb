1f6887791ae9b85ef0d64a2ebbd14c4308568031
Bala Paranj <bparanj@onekingslane.com>
2016-05-16 17:06:08 -0700

    Implement search feature using searchkick

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,13 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
 
+  def search
+    @movies = if params[:term].present?
+      Movie.search(params[:term])
+    else
+      Movie.all
+    end
+  end
   # GET /movies
   # GET /movies.json
   def index
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
+  searchkick
   has_attached_file :poster, styles: { medium: "400x600#" }
   validates_attachment_content_type :poster, content_type: /\Aimage\/.*\Z/
 end
